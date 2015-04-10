bff593ea707bc514ee193f57a07f9d820ea40781
TzepART <artem060890@gmail.com>
2015-04-10 18:03:12 +0400

    Create main page

---
+++ resources/views/index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>InstaTranslate</title>
+
+        <!-- подключение стилей -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+	</head>
+
+
+    <body>
+		<div class="container">
+			<div class="content">
+				<div class="title">Welcome to instatranslate</div>
+				<div class="quote"><!-- Indicates a successful or positive action -->
+                    <button type="button" class="btn btn-success btn-lg main-btn">Login</button>
+                </div>
+			</div>
+		</div>
+	</body>
+</html>
