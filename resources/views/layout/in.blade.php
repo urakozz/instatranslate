cd350cb46e323ada86590dee4e3cc2dddcaf1cf3
Yury Kozyrev <yury@locadmins-MacBook-Pro.local>
2015-04-13 14:05:22 +0200

    metrika

--- resources/views/layout/in.blade.php
+++ resources/views/layout/in.blade.php
@@ -23,5 +23,30 @@
     <div class="container it-content">
         @yield('content')
     </div>
+    @if(App::environment('production'))
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter29693235 = new Ya.Metrika({id:29693235});
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                        s = d.createElement("script"),
+                        f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="//mc.yandex.ru/watch/29693235" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+    @endif
 </body>
 @stop
