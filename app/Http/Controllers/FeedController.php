36a5e67fadc280747510e3716f2babd6a1e12e80
Yury Kozyrev <yury.kozyrev@home24.de>
2015-04-28 00:02:13 +0200

    fix

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -89,8 +89,8 @@ class FeedController extends Controller
 
         \Log::info('Translator call start');
         $t          = microtime(true);
-        $translator = new Translator(Guzzle::getFacadeRoot(), new BingTranslator());
-//        $translator->setCache(new LaravelDoctrineCache());
+        $translator = new Translator(Guzzle::getFacadeRoot(), new YandexTranslator());
+        $translator->setCache(new LaravelDoctrineCache());
         $translator->translate(new InstagramAdapter($data));
         \Log::info(sprintf("Translator call end, time is %.04F", microtime(true) - $t));
 
