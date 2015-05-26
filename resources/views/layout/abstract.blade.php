48babe85fd99798fba6910644e99cf74e09c8824
urakozz <urakozz@gmail.com>
2015-05-26 16:29:25 +0200

    icon

--- resources/views/layout/abstract.blade.php
+++ resources/views/layout/abstract.blade.php
@@ -3,6 +3,7 @@
     <title>InstaTranslate @yield('title')</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="//instagramstatic-a.akamaihd.net/bluebar/660508e/images/ico/favicon.ico">
 
     <link href="/css/app.css" rel="stylesheet">
     <link href='//fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
