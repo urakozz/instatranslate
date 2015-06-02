7b8cd932546202dcec25800ebd03fd4e33d87aa0
urakozz <urakozz@gmail.com>
2015-06-02 11:46:17 +0200

    updates + 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.promo')
+@section('content')
+    <div class="promo title">
+        Invalid page
+    </div>
+    <div class="promo quote">
+        <a role="button" class="btn btn-success btn-lg it-button" href="/">Maybe try again? Go to the main page</a>
+    </div>
+@stop
+@stop
